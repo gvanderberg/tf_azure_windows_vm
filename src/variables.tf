33e4ebabe0c9fc588deade7623c27e7f3be35c77
Gareth Van Der Berg <vanderbergg@out.co.za>
2020-01-21 16:07:31 +0200

    update tf deployment steps

--- src/variables.tf
+++ src/variables.tf
@@ -1,23 +1,3 @@
-# -------------------------------------------
-
-variable "client_id" {
-  default = "__client_id__"
-}
-
-variable "client_secret" {
-  default = "__client_secret__"
-}
-
-variable "subscription_id" {
-  default = "__subscription_id__"
-}
-
-variable "tenant_id" {
-  default = "__tenant_id__"
-}
-
-# -------------------------------------------
-
 variable "admin_username" {
   default = "__admin_username__"
 }
